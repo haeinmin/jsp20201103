41873526bafccf8fe812c1bf058a9f56125bdd0a
haeinmin <haeinmin29@gmail.com>
2020-12-01 16:44:52 +0900

    sql

---
+++ src/chap14/servlet/AddEmployeeServlet.java
@@ -0,0 +1,79 @@
+package chap14.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddEmployeeServlet
+ */
+@WebServlet("/sample/employee/add")
+public class AddEmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddEmployeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String eno = request.getParameter("eno");
+		String ename = request.getParameter("ename");
+		String dno = request.getParameter("dno");
+		
+		String sql = "INSERT INTO employee (eno, ename, dno) VALUES (?, ?, ?)";
+		
+		String url = "jdbc:mysql://localhost:3306/lecture?serverTimezone=Asia/Seoul";
+		String id = "root";
+		String pw = "adminAdmin1";
+		
+		try {
+			// 1. load driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 2. db connection 구함
+			Connection con = DriverManager.getConnection(url, id, pw);
+			
+			//3. create statement
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, Integer.parseInt(eno));
+			pstmt.setString(2, ename);
+			pstmt.setInt(3, Integer.parseInt(dno));
+			
+			//4. run query
+			pstmt.executeUpdate();
+			
+			//5. process
+			
+			//6. close statement, connection
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
